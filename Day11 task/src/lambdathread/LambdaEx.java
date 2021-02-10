2069ad2f4ada14db1cccd3faf616c52624f65bc6
bhagyasree <bhagyasreeguntuku@gmail.com>
2021-02-10 21:05:56 +0530

    commit

---
+++ Day11 task/src/lambdathread/LambdaEx.java
@@ -0,0 +1,27 @@
+package lambdathread;
+
+
+@FunctionalInterface
+interface good
+{
+	public void display(int...num);
+}
+public class LambdaEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		good g=(num)->{
+			int sum=0;
+			for(int  i:num)
+			{
+				sum=sum+i;
+				
+			}
+			System.out.println(sum);
+		};
+		g.display(12,78,45,23);
+		g.display(12,78,45,23,11);
+	}
+	}
+
